af3e23eb2155660ae1fc5ad5fd9877e21412fc8f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-12 14:43:38 +0900

    とりあえずメインで扱うクラスとasmdefを用意

---
+++ Assets/Plugins/AppleAuthExtension/Scripts/AppleAuth.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AppleAuthExtension
+{
+    /// <summary>
+    /// Apple認証
+    /// </summary>
+    public class AppleAuth : MonoBehaviour
+    {
+    }
+}
