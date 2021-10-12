faf2189490bd3982e56f5cd3b7980df6ff2df10a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-12 15:05:01 +0900

    サポートされていないプラットフォームで実行しようとした場合に投げられる例外

---
+++ Assets/Plugins/AppleAuthExtension/Scripts/Exceptions/AppleAuthNotSupportedException.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace AppleAuthExtension
+{
+    /// <summary>
+    /// サポートされていないプラットフォームで実行しようとした時に投げられる例外
+    /// </summary>
+    public class AppleAuthNotSupportedException : Exception
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public AppleAuthNotSupportedException()
+            : base("このプラットフォームはサポートされていません")
+        {
+        }
+    }
+}
