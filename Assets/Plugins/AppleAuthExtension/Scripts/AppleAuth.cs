fdf619c707cf4e0d7da7f91b64242d203ed50936
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-12 15:07:59 +0900

    認証処理を実装

--- Assets/Plugins/AppleAuthExtension/Scripts/AppleAuth.cs
+++ Assets/Plugins/AppleAuthExtension/Scripts/AppleAuth.cs
@@ -1,6 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cysharp.Threading.Tasks;
+using AppleAuth;
+using AppleAuth.Interfaces;
+using System.Threading;
+using AppleAuth.Enums;
+using AppleAuth.Native;
 
 namespace AppleAuthExtension
 {
@@ -9,5 +15,63 @@ namespace AppleAuthExtension
     /// </summary>
     public class AppleAuth : MonoBehaviour
     {
+        /// <summary>
+        /// CompletionSource
+        /// </summary>
+        private UniTaskCompletionSource<ICredential> completionSource = null;
+
+        /// <summary>
+        /// 認証マネージャ
+        /// </summary>
+        private AppleAuthManager authManager = null;
+
+        /// <summary>
+        /// サインイン
+        /// </summary>
+        /// <param name="logInOptions">ログインオプション</param>
+        /// <param name="nonce">ノンス</param>
+        /// <param name="token">CancellationToken</param>
+        /// <returns>ICredential</returns>
+        public static UniTask<ICredential> SignIn(LoginOptions logInOptions, string nonce = null, CancellationToken token = default)
+        {
+            GameObject obj = new GameObject("AppleAuth");
+            var auth = obj.AddComponent<AppleAuth>();
+
+            auth.completionSource = new UniTaskCompletionSource<ICredential>();
+            auth.authManager = new AppleAuthManager(new PayloadDeserializer());
+            auth.authManager.LoginWithAppleId(new AppleAuthLoginArgs(logInOptions, nonce), credential =>
+            {
+                auth.completionSource.TrySetResult(credential);
+                Destroy(obj);
+            },
+            error =>
+            {
+                AppleAuthErrorException exception = new AppleAuthErrorException(error);
+                auth.completionSource.TrySetException(exception);
+                Destroy(obj);
+                throw exception;
+            });
+            return auth.completionSource.Task;
+        }
+
+        void Start()
+        {
+            if (!AppleAuthManager.IsCurrentPlatformSupported)
+            {
+                var exception = new AppleAuthNotSupportedException();
+                completionSource.TrySetException(exception);
+                Destroy(gameObject);
+                throw exception;
+            }
+        }
+
+        void Update()
+        {
+            if (authManager != null)
+            {
+                authManager.Update();
+            }
+        }
     }
 }
+
