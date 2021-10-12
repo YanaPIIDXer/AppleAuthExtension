4ea2b9e7935e61f733bb2bd85b5033c722ad28a3
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-12 15:19:30 +0900

    メイン画面

---
+++ Assets/Sandbox/Scripts/MainScreen.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+/// <summary>
+/// メイン画面
+/// </summary>
+public class MainScreen : MonoBehaviour
+{
+    /// <summary>
+    /// 結果のテキスト
+    /// </summary>
+    [SerializeField]
+    private Text resultText = null;
+
+    /// <summary>
+    /// 認証ボタン
+    /// </summary>
+    [SerializeField]
+    private Button authButton = null;
+
+    /// <summary>
+    /// 認証ボタンが押された
+    /// </summary>
+    public Action OnAuth { set; private get; }
+
+    /// <summary>
+    /// 結果のテキストを設定
+    /// </summary>
+    /// <param name="text">テキスト</param>
+    public void SetResultText(string text)
+    {
+        resultText.text = text;
+        authButton.interactable = true;
+    }
+
+    /// <summary>
+    /// 認証ボタンが押された
+    /// </summary>
+    public void OnClickAuthButton()
+    {
+        authButton.interactable = false;
+        OnAuth?.Invoke();
+    }
+}
