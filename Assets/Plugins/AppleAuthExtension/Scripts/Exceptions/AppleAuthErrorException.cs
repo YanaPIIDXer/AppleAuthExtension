60251703326428df110ba9648dec4aa8f426106d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-12 15:07:52 +0900

    認証エラー時に発生する例外

---
+++ Assets/Plugins/AppleAuthExtension/Scripts/Exceptions/AppleAuthErrorException.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using AppleAuth;
+using AppleAuth.Interfaces;
+
+namespace AppleAuthExtension
+{
+    /// <summary>
+    /// 認証エラーの例外
+    /// </summary>
+    public class AppleAuthErrorException : Exception
+    {
+        /// <summary>
+        /// エラーインタフェース
+        /// </summary>
+        public IAppleError Error { get; private set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="error">エラーインタフェース</param>
+        public AppleAuthErrorException(IAppleError error)
+            : base("認証エラー")
+        {
+            Error = error;
+        }
+    }
+}
